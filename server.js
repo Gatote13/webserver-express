18974776e060d7b290ee33ce6215137773e87aa9
Dario <dario.slps@gmail.com>
2018-11-15 17:12:00 -0600

    primer commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+
+const port = process.env.PORT || 3000;
+
+app.use( express.static( __dirname+'/public') );
+
+
+//espress HBS 
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+ 
+
+
+
+
+
+
+app.get('/',  (req, res) => {
+
+  res.render('home',{
+    nombre: 'Dario'
+  });
+ 
+});
+
+app.get('/about',  (req, res) => {
+
+  res.render('about',{});
+ 
+});
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${port}`)
+});
